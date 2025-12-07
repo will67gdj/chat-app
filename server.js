829a814e078f98779bcd29f9c58994ecd236e300
will67gdj <willeszzer@gmail.com>
2025-12-06 19:53:42 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require("ws");
+
+const port = process.env.PORT || 3000;
+
+const wss = new WebSocket.Server({ port });
+
+console.log("Chat server running on port " + port);
+
+wss.on("connection", (ws) => {
+    ws.on("message", msg => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        });
+    });
+}); and 
